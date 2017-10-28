c0e86c7a4e17fccb7d179c0457fb00701399d950
drstonebraker <drew.stonebraker@gmail.com>
2017-10-27 23:55:01 -0700

    Initial commit

---
+++ bench_bnb_jest_practice/frontend/actions/__tests__/bench_actions-test.js
@@ -0,0 +1,31 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from '../bench_actions';
+import * as ApiUtil from '../../util/bench_api_util';
+
+import { testBenches } from '../../testUtil/bench_helper';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('async actions', () => {
+
+  test('fetchBenches creates RECEIVE_BENCHES after fetching benches', () => {
+    // REFER TO REDUX TESTS DOCS
+    // Set up expectedActions:
+    // Your code here
+
+    ApiUtil.fetchBenches = jest.fn(() => {
+      return Promise.resolve(testBenches);
+    });
+
+    const store = mockStore({ benches: {} });
+    const expectedActions = [
+      { type: actions.RECEIVE_BENCHES, benches: testBenches },
+    ]
+
+    return store.dispatch(actions.fetchBenches()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    });
+  });
+});
